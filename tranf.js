a698095dbbca72bd47e068f70fa628f2365e95d0
toan tran <tqthl.99@gmail.com>
2020-11-27 15:57:33 +0700

    first commit

---
+++ tranf.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    }
+    else{
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+    
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
